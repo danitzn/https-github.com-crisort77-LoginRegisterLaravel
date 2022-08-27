8f7e962b130d8c3683f7cea8cdb175e0811fa83b
Cristian Ortega <crisarielorte@gmail.com>
2022-08-27 17:10:59 -0400

    titulos descripciones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     
-  <h1 class="text-5xl text-center pt-24">Welcome to my application</h1>
+  <h1 class="text-5xl text-center pt-24">Sistema de Gestión de Proyecto Ágil</h1>
 
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>@yield('title') - Laravel App</title>
+    <title>@yield('title') - SGDPA</title>
     
     <!-- Tailwind CSS Link -->
     <link rel="stylesheet" 
@@ -14,27 +14,27 @@
 
     <nav class="flex py-5 bg-indigo-500 text-white">
       <div class="w-1/2 px-12 mr-auto">
-        <p class="text-2xl font-bold">My Application</p>
+        <p class="text-2xl font-bold">SGDPA</p>
       </div>
 
       <ul class="w-1/2 px-16 ml-auto flex justify-end pt-1">
       @if(auth()->check())
         <li class="mx-8">
-          <p class="text-xl">Welcome <b>{{ auth()->user()->name }}</b></p>
+          <p class="text-xl">Bienvenido <b>{{ auth()->user()->name }}</b></p>
         </li>
         <li>
           <a href="{{ route('login.destroy') }}" class="font-bold
-          py-3 px-4 rounded-md bg-red-500 hover:bg-red-600">Log Out</a>
+          py-3 px-4 rounded-md bg-red-500 hover:bg-red-600">Salir</a>
         </li>
       @else
         <li class="mx-6">
           <a href="{{ route('login.index') }}" class="font-semibold 
-          hover:bg-indigo-700 py-3 px-4 rounded-md">Log In</a>
+          hover:bg-indigo-700 py-3 px-4 rounded-md">Inicicar Sesión</a>
         </li>
         <li>
           <a href="{{ route('register.index') }}" class="font-semibold
           border-2 border-white py-2 px-4 rounded-md hover:bg-white 
-          hover:text-indigo-700">Register</a>
+          hover:text-indigo-700">Registrarse</a>
         </li>
       @endif
       </ul>
