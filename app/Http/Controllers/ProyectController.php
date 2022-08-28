a0dc66116500236efa3e0fc95410c30f2ff45e6a
Soy_Doctor <danitzn@gmail.com>
2022-08-28 20:35:20 -0300

    agg_proyecto_controller

---
+++ app/Http/Controllers/ProyectController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Proyect;
+
+class ProyectController extends Controller {
+    
+    public function create() {
+        
+        return view('auth.proyect');
+    }
+
+    public function store() {
+
+        $this->validate(request(), [
+            'name' => 'required',
+            'descripcion' => 'required|email',
+            'inicio' => 'inicio'
+            'usuario' => 'confirmed'
+        ]);
+
+        $user = Proyect::create(request(['name', 'descripcion', 'inicio','usuario']));
+
+       /* auth()->login($user);*/
+        return redirect()->to('/');
+    }
+}
